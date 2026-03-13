81fa1239424f4a19bb63f63b520c89efb1057f3b
mdz48 <107087433+mdz48@users.noreply.github.com>
2026-03-13 13:03:36 -0600

    3er hardware incorporado (#9)
    Co-authored-by: Maximiliano Diaz Clemente <233306@ids.upchiapas.edu.mx>

--- app/src/main/java/com/alilopez/kt_demohilt/core/di/HardwareModule.kt
+++ app/src/main/java/com/alilopez/kt_demohilt/core/di/HardwareModule.kt
@@ -3,9 +3,11 @@ package com.alilopez.kt_demohilt.core.di
 import com.alilopez.kt_demohilt.core.hardware.data.AndroidCameraPhotoManager
 import com.alilopez.kt_demohilt.core.hardware.data.AndroidFlashManager
 import com.alilopez.kt_demohilt.core.hardware.data.AndroidMicrophoneManager
+import com.alilopez.kt_demohilt.core.hardware.data.AndroidSoundManager
 import com.alilopez.kt_demohilt.core.hardware.domain.CameraPhotoManager
 import com.alilopez.kt_demohilt.core.hardware.domain.FlashManager
 import com.alilopez.kt_demohilt.core.hardware.domain.MicrophoneManager
+import com.alilopez.kt_demohilt.core.hardware.domain.SoundManager
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -33,4 +35,10 @@ abstract class HardwareModule {
     abstract fun bindMicrophoneManager(
         impl: AndroidMicrophoneManager
     ): MicrophoneManager
+
+    @Binds
+    @Singleton
+    abstract fun bindSoundManager(
+        impl: AndroidSoundManager
+    ): SoundManager
 }
